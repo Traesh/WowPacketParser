398900820c16e10b244bdf0666068ede8a21a61c
ModoX <moardox@gmail.com>
2018-09-21 02:23:10 +0200

    added V8_0_1_27791

--- WowPacketParser/Misc/ClientVersion.cs
+++ WowPacketParser/Misc/ClientVersion.cs
@@ -238,6 +238,7 @@ namespace WowPacketParser.Misc
             new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V8_0_1_27481, new DateTime(2018, 08, 28)),
             new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V8_0_1_27547, new DateTime(2018, 08, 31)),
             new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V8_0_1_27602, new DateTime(2018, 09, 05)),
+            new KeyValuePair<ClientVersionBuild, DateTime>(ClientVersionBuild.V8_0_1_27791, new DateTime(2018, 09, 20)),
         };
 
         private static ClientType _expansion;
@@ -480,6 +481,7 @@ namespace WowPacketParser.Misc
                     case ClientVersionBuild.V8_0_1_27481:
                     case ClientVersionBuild.V8_0_1_27547:
                     case ClientVersionBuild.V8_0_1_27602:
+                    case ClientVersionBuild.V8_0_1_27791:
                         return ClientVersionBuild.V8_0_1_27101;
                     case ClientVersionBuild.BattleNetV37165:
                         return ClientVersionBuild.BattleNetV37165;
